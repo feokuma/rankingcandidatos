14366e1d024c1ce3b5da958bf93d3695ba8e13db
Fernando Okuma <feokuma@gmail.com>
2021-02-03 00:45:14 -0300

    :white_check_mark: Cria servidor do teste de integração

---
+++ tests/rankingcandidatos.integration/BaseTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using NUnit.Framework;
+using rankingcandidatos.API;
+using System.IO;
+using System.Net.Http;
+
+namespace rankingcandidatos.integration
+{
+    public class BaseTest
+    {
+        public WebApplicationFactory<Startup> factory { get; private set; }
+        public HttpClient httpClient { get; private set; }
+
+        [OneTimeSetUp]
+        public void SetupWebHost()
+        {
+            factory = new WebApplicationFactory<Startup>().WithWebHostBuilder(builder => 
+            {
+                builder.UseEnvironment("Test");
+            });
+
+            httpClient = factory.CreateClient();
+        }
+    }
+}
---
+++ tests/rankingcandidatos.integration/specs/WeatherForecastControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace rankingcandidatos.integration.specs
+{
+    public class WeatherForecastControllerTests : BaseTest
+    {
+        [Test]
+        public async Task GetShouldReturnStatusCode200Ok()
+        {
+            var response = await httpClient.GetAsync("/WeatherForecast");
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+        }
+    }
+}
