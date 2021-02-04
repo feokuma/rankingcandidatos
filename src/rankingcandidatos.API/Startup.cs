a1992e483ce92a4fdf8e740e30fcfaf50b6288d2
Fernando Okuma <feokuma@gmail.com>
2021-02-04 00:06:34 -0300

    :building_construction: Adiciona Swagger ao projeto

--- src/rankingcandidatos.API/Startup.cs
+++ src/rankingcandidatos.API/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using rankingcandidatos.API.Infra.Dados;
 
 namespace rankingcandidatos.API
@@ -29,6 +30,11 @@ namespace rankingcandidatos.API
 
             services.AddSingleton<ICandidatoRepositório, CandidatoRepositório>();
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RankingCandidatosAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +50,10 @@ namespace rankingcandidatos.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RankingCandidatosAPI"));
 
             app.UseEndpoints(endpoints =>
             {
